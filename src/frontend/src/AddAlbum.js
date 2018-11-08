6c2f47ace9c728fe86dd88edc7db1c796c5f38f7
rorscharg <arguinjr@gmail.com>
2018-11-07 19:11:02 -0500

    Add frontend navigation + Header

---
+++ src/frontend/src/AddAlbum.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AddAlbum extends Component {
+  render() {
+    return (
+      <div>
+        Add albumn
+      </div>
+    );
+  }
+}
+
+export default AddAlbum;
