3aca9c2b0d940127a28e48b80079be872731b447
Marc Deslandes <marcdeslandez@gmail.com>
2018-11-14 15:08:57 -0500

    Added dependancies for album creation

--- src/backend/test.py
+++ src/backend/test.py
@@ -4,12 +4,7 @@ import os
 appPath = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + "/../..")
 sys.path.append(appPath)
 
-from src.analysis.choose import choose_photos
+from src.album.creator import create_album
 from src.analysis.choose import Warmth
 
-sauce = choose_photos(appPath + "/res", 5, Warmth.COLD, True)
-
-print(sauce)
-
-for val in sauce:
-    print(val)
+sauce = create_album(appPath + "/res", "title1", 5, Warmth.COLD, True)
