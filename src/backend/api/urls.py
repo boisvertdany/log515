37e58742abb5afcd073c1c31f534f1b1e86c2b78
Marc Deslandes <marcdeslandez@gmail.com>
2018-11-14 11:02:10 -0500

    URLs update

--- src/backend/api/urls.py
+++ src/backend/api/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.UserListView.as_view()),
-    path('rest-auth/', include('rest_auth.urls')),
-    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('auth/', include('rest_auth.urls')),
+    path('auth/register/', include('rest_auth.registration.urls')),
     path('album/', views.ListAlbum.as_view()),
     path('album/<int:pk>/', views.DetailAlbum.as_view()),
 ]
