3e619b33fd0a94b2bf7cd0901fe4ab86677daca5
Marc Deslandes <marcdeslandez@gmail.com>
2018-11-13 21:54:03 -0500

    Authentication scheme setup

--- src/backend/pixnmatch/settings.py
+++ src/backend/pixnmatch/settings.py
@@ -53,6 +53,16 @@ SITE_ID = 1
 
 AUTH_USER_MODEL = 'api.CustomUser'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
